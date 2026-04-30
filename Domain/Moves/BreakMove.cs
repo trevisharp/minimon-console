b42a0aa2c230c3f9878672d114040b1762a259de
trevisharp <leonardotrevisansilio@gmail.com>
2026-04-30 20:17:58 -0300

    unchange

--- Domain/Moves/BreakMove.cs
+++ Domain/Moves/BreakMove.cs
@@ -4,7 +4,7 @@ public class BreakMove() : Move("Break", Type.Normal, -1, 1)
 {
     protected override bool HandleMove(BattleContext ctx)
     {
-        ctx.Enemy.Recive(DamageType.Strong, Type.Normal, 5);
+        ctx.Enemy.Recive(DamageType.Strong, Type.Normal, 4);
         return true;
     }
 }
